9631cf784d5809b5866b6e29922e62423167c8ab
文燚 <yshenhua@gmail.com>
2018-06-16 18:00:05 +0800

    源码

---
+++ README.md
@@ -0,0 +1,23 @@
+# vue2-jtodo
+一个简单的 todo 应用
+
+## 下载运行
+``` bash
+# download the project
+git clone https://github.com/yshenhua/vue2-jtodo.git
+
+# install dependencies
+cd vue2-jtodo && npm install
+
+# serve with hot reload at localhost:3010
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+## 在线预览
+<https://yshenhua.github.io/vue2-jtodo>
---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <Header/>
+    <Todo/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+import Todo from './components/Todo'
+import Footer from './components/Footer'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Todo,
+    Footer
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#app {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  &::before {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+    background-color: #999;
+    opacity: .9;
+  }
+}
+</style>
